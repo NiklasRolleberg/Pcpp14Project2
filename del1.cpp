e87f44fd599b8e19a8002cf654a29c4a5c07bf2c
hannahx <milger@kth.se>
2014-10-23 16:58:43 +0200

    del1

--- del1.cpp
+++ del1.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <cmath>
+
+double myexp(double x, double tol);
 
 int main()
 {
-	std::cout << "Hej" << std::endl;
+
+    std::cout << myexp(975, 1e-10);
+
+
 	return 0;
+}
+
+
+double myexp(double x, double tol)
+{
+    int N = 10;
+    double myExp = 0;
+    double expX = exp(x);
+    while (fabs(myExp - expX) > tol)
+    {
+        myExp = 1;
+        for(int i=N; i>0; i--)
+        {
+            myExp *= x/i;
+            myExp += 1.;
+        }
+        //std::cout << myExp << std::endl;
+        N += 10;
+    }
+    return myExp;
 }
